37e2d1558202062093a5d3a2426458d5154e2b2c
unknown <[nicksonnenberg95@gmail.com]>
2021-09-30 17:47:22 -0400

    initial commit

---
+++ src/business/TodoForm.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react'
+
+function TodoForm(props) {
+const [input, setInput] = useState('');
+
+const handleChange = e => {
+    setInput(e.target.value);
+}
+
+const handleSubmit = e => {
+    e.preventDefault();
+
+    props.onSubmit({
+        id: Math.floor(Math.random() * 1000),
+        text: input
+  });
+  
+  setInput("");
+};
+    return (
+        <div>
+           <form className="Todo-form" onSubmit={handleSubmit}>
+               <input type="text" placeholder="Add a task" value={input}
+               name="text" className="todo-input" onChange={handleChange} />
+               <button className="Todo-Button">
+                    Add Task
+                </button>
+            </form> 
+        </div>
+    )
+};
+
+export default TodoForm
